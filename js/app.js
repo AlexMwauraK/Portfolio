87415dd97b796e3a404502c592a4d34ab6dda507
AlexMwauraK <alecsmwaura@gmail.com>
2022-11-17 04:48:29 +0300

    page one Complete

--- js/app.js
+++ js/app.js
@@ -1,13 +1,18 @@
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.navlinks');
-    const navlinks = document.querySelectorAll('.navlinks li')
-    burger.addEventListener('click', () =>{
-        nav.classList.toggle('nav-active');
-    });
-    navlinks.forEach((link, index) => {
-      link.style.animation = 'navLinkFade 0.5s ease forwards ${index / 7 + 2}s';
-    //   console.log(index / 5 + 0.2);
+    const navlinks = document.querySelectorAll('.navlinks li');
+
+    burger.addEventListener('click', () => {
+      nav.classList.toggle('nav-active');
+      navlinks.forEach((link, index) => {
+        if (link.style.animation) {
+          link.style.animation = ''
+        }else{
+          link.style.animation = `navlinkFade 0.5s ease forward ${index / 7 + 4 }s`;
+        } 
+      });
+      burger.classList.toggle('toggles');
     });
 }
 navSlide();
