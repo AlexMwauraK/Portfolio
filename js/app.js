e776dfa67cab5bf5e4cb9f729481184b7af06598
AlexMwauraK <alecsmwaura@gmail.com>
2023-03-02 02:10:33 +0300

    fuck

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
+//Menu Burger homepage
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.navlinks');
+  const navlinks = document.querySelectorAll('.navlinks li');
+
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+    navlinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = ''
+      }else{
+        link.style.animation = `navlinkFade 0.5s ease forward ${index / 7 + 4 }s`;
+      } 
+    });
+    burger.classList.toggle('toggles');
+  });
+}
+navSlide();
+//Auto type
 var typed = new Typed('.element', {
     strings: ["Front-End Developer", "Photographer", "ui/ux Designer"],
     typeSpeed: 50,
