bbfefc4a0bbb9cc23a1f2f015e5410c9406343ae
Travis CI <travis@travis-ci.org>
2023-03-30 18:57:17 +0300

    update

--- js/app.js
+++ js/app.js
@@ -19,7 +19,7 @@ const navSlide = () => {
 navSlide();
 //Auto type
 var typed = new Typed('.element', {
-    strings: ["Front-End Developer", "Photographer", "ui/ux Designer", "Graphic Designer"],
+    strings: ["Front-End Developer", "Photographer", "ui/ux Designer"],
     typeSpeed: 50,
     backSpeed: 70,
     loop: true
